31f923e1edf2758019f6f62cd48dc7a2986312df
sHMENGGo <158861993+sHMENGGo@users.noreply.github.com>
2025-08-02 02:18:29 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+firstCapy = document.querySelector('.firstCapy');
+secondCapy = document.querySelector('.secondCapy');
+thirdCapy = document.querySelector('.thirdCapy');
+const firstCard = document.querySelector('.firstCard');
+const secondCard = document.querySelector('.secondCard');
+const thirdCard = document.querySelector('.thirdCard');
+const fourthCard = document.querySelector('.fourthCard');
+const answer = document.querySelector('#answer');
+const nextButton = document.querySelector('.nextButton');
+const submit = document.querySelector('#submit');
+okay = document.querySelector('.okay');
+okay2 = document.querySelector('.okay2');
+
+correctAnswer = "Capy";
+
+submit.addEventListener('click', () => {
+    if (answer.value.toLowerCase() === correctAnswer.toLowerCase()) {
+        secondCard.classList.add('slideNext');
+        firstCapy.classList.replace('block', 'hidden');
+        secondCapy.classList.replace('hidden', 'block');
+        setTimeout(() => {
+            firstCard.classList.add('hidden');
+        }, 900);
+    } else {
+        alert("WRONG ANSWER!!!");
+    }
+});
+
+okay.addEventListener('click', () => {
+    setTimeout(() => {
+            secondCard.classList.add('hidden');
+        }, 900);
+
+        thirdCard.classList.add('slideNext');
+});
+
+okay2.addEventListener('click', () => {
+    setTimeout(() => {
+        thirdCard.classList.add('hidden');
+    }, 900);
+
+    secondCapy.classList.replace('block', 'hidden');
+    thirdCapy.classList.replace('hidden', 'block');
+    fourthCard.classList.add('slideNext');
+});
+
